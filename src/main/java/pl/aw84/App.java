46f192556f227d09571dc72f0281b47470381af8
aw84 <silencewalker@gmail.com>
2018-05-03 18:06:56 +0200

    Initial commit

---
+++ src/main/java/pl/aw84/App.java
@@ -0,0 +1,13 @@
+package pl.aw84;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
